9633895e19f68540e457c62e5ae7376345f79065
trangqle <69665159+trangqle@users.noreply.github.com>
2022-07-11 19:56:34 -0500

    new.js
    absolute mad man reqriting the entire thing for fun wth plz help

--- js/main.js
+++ js/main.js
@@ -1,139 +1,154 @@
+let docObjects = [];
+let domAttrAdd = new Map();
+let domAttrRemove = new Map();
+let domSetStyle = new Map();
+let domSetStyleProp = new Map();
+let afterFrame = [];
+
+let frameId = 0;
+let lastFrameTime = 0;
+function renderFrame(frameTime) {
+    for (const [idx, elem] of domAttrAdd)
+        docObjects[idx].setAttribute(elem, '');
+    for (const [idx, elem] of domSetStyle)
+        docObjects[idx].setAttribute('style', elem);
+    for (const [idx, [prop, value]] of domSetStyleProp)
+        docObjects[idx].style.setProperty(prop, value);
+    for (const [idx, elem] of domAttrRemove)
+        docObjects[idx].removeAttribute(elem);
+
+    domAttrAdd.clear();
+    domAttrRemove.clear();
+    domSetStyle.clear();
+    domSetStyleProp.clear();
+
+    frameId = 0;
+
+    let localAfterFrame = [...afterFrame];
+    afterFrame.splice(0);
+    for (let idx = 0; idx < localAfterFrame.length; idx++)
+        localAfterFrame[idx](frameTime);
+
+    lastFrameTime = frameTime;
+}
+function queueFrame() {
+    frameId = frameId === 0 ? requestAnimationFrame(renderFrame) : frameId;
+}
+
+const DocId = docObjects.push(document.documentElement) - 1;
+
+let instantCursors = [];
+let lerpCursors = [];
+for (cursor of document.getElementsByClassName("cursor")) {
+    if (cursor.classList.contains('cursor-anim:lerp')) {
+        lerpCursors.push(docObjects.push(cursor) - 1);
+    } else {
+        instantCursors.push(docObjects.push(cursor) - 1);
+    }
+}
+let lerpPosition = lerpCursors.map(() => [0, 0]);
+
+function setCursorPosition(id, x, y) {
+    domSetStyle.set(id, `--x: ${x}px; --y: ${y}px`);
+}
+
+function lerp(src, dest, step) {
+    return src + step * (dest - src);
+}
+
+let lerpAnimateId = -1;
+let lerpTarget = [0, 0];
+function animateLerpCursors(frameTime) {
+    lerpAnimateId = -1;
+
+    for (let idx = 0; idx < lerpCursors.length; idx++) {
+        const id = lerpCursors[idx];
+        if (Math.abs(lerpPosition[idx][0] - lerpTarget[0]) > 0.1
+            || Math.abs(lerpPosition[idx][1] - lerpTarget[1]) > 0.1) {
+            lerpPosition[idx][0] = lerp(lerpPosition[idx][0], lerpTarget[0], 0.5 * Math.min(1, (frameTime - lastFrameTime) / 50));
+            lerpPosition[idx][1] = lerp(lerpPosition[idx][1], lerpTarget[1], 0.5 * Math.min(1, (frameTime - lastFrameTime) / 50));
+
+            setCursorPosition(id, lerpPosition[idx][0], lerpPosition[idx][1]);
+
+            queueLerpCursorAnimation();
+            queueFrame();
+        }
+    }
+}
+
+function queueLerpCursorAnimation() {
+    lerpAnimateId = lerpAnimateId < 0 ? afterFrame.push(animateLerpCursors) - 1 : lerpAnimateId;
+}
+
+function enableCursors() {
+    domAttrAdd.set(DocId, 'data-customcursor');
+    domAttrRemove.delete(DocId);
+}
+
+function disableCursors() {
+    domAttrRemove.set(DocId, 'data-customcursor');
+    domAttrAdd.delete(DocId);
+}
+
+function setHover(id) {
+    domAttrAdd.set(id, 'data-hover');
+    domAttrRemove.delete(id);
+}
+
+function setCursorHover() {
+    for (let idx = 0; idx < instantCursors.length; idx++)
+        setHover(instantCursors[idx]);
+    for (let idx = 0; idx < lerpCursors.length; idx++)
+        setHover(lerpCursors[idx]);
+}
+
+function unsetHover(id) {
+    domAttrRemove.set(id, 'data-hover');
+    domAttrAdd.delete(id);
+}
+
+function unsetCursorHover() {
+    for (let idx = 0; idx < instantCursors.length; idx++)
+        unsetHover(instantCursors[idx]);
+    for (let idx = 0; idx < lerpCursors.length; idx++)
+        unsetHover(lerpCursors[idx]);
+}
+
+function handlePointer(event) {
+    if (event.pointerType !== 'touch') {
+        if (event.type !== 'pointerleave')
+            enableCursors();
+        else
+            disableCursors();
+
+        if (event.type === 'pointerover')
+            setCursorHover();
+        else if (event.type === 'pointerout')
+            unsetCursorHover();
+
+        for (let idx = 0; idx < instantCursors.length; idx++)
+            setCursorPosition(instantCursors[idx], event.clientX, event.clientY);
+
+        lerpTarget = [event.clientX, event.clientY];
+        queueLerpCursorAnimation();
+        queueFrame();
+    }
+}
+
+docObjects[DocId].addEventListener('pointerenter', handlePointer, {passive: true});
+docObjects[DocId].addEventListener('pointermove', handlePointer, {passive: true});
+docObjects[DocId].addEventListener('pointerleave', handlePointer, {passive: true});
+for (const elem of document.querySelectorAll('a, button')) {
+    elem.addEventListener('pointerover', handlePointer, {passive: true});
+    elem.addEventListener('pointerout', handlePointer, {passive: true});
+}
+
 let header = document.getElementById("header");
 if (header) {
     new ResizeObserver((entries) => {
         let box = entries[0];
         let blockSize = box.borderBoxSize ? box.borderBoxSize[0].blockSize : box.target.getBoundingClientRect().height;
-        document.documentElement.style.setProperty('--header-block-size', `${blockSize}px`);
+        domSetStyleProp.set(DocId, ['--header-block-size', `${blockSize}px`]);
+        queueFrame();
     }).observe(header, {box: 'border-box'});
-}
-
-let cursors = Array.from(document.getElementsByClassName("cursor"));
-let pointerPos = {x: 0, y: 0};
-let cursorPos = cursors.map(() => {return {x: 0, y: 0};});
-
-let enabledCursors = false;
-let nextCursorState = enabledCursors;
-let hoverState = false;
-let nextHoverState = hoverState;
-
-let rendererId = 0;
-let updateCursorIds = new Array();
-let cursorIds = new Array();
-let lerpCursorIds = new Array();
-
-for (const id of cursors.keys()) {
-    let cursor = cursors[id];
-    if (cursor.classList.contains('cursor-anim:lerp')) {
-        lerpCursorIds.push(id);
-    } else {
-        cursorIds.push(id);
-    }
-}
-
-let lastUpdate = 0;
-
-let renderCursors = () => {
-    if (nextCursorState !== enabledCursors) {
-        for (let i = 0; nextCursorState && i < cursorPos.length; i++) {
-            cursorPos[i].x = pointerPos.x;
-            cursorPos[i].y = pointerPos.y;
-            if (!updateCursorIds.includes(i))
-                updateCursorIds.push(i);
-        }
-        if (enabledCursors) {
-            document.documentElement.removeAttribute('data-customcursor');
-        } else {
-            document.documentElement.setAttribute('data-customcursor', '');
-        }
-
-        enabledCursors = nextCursorState;
-    }
-
-    while (updateCursorIds.length > 0) {
-        let id = updateCursorIds.pop();
-        // setAttribute is faster than everything else
-        cursors[id].setAttribute('style', `--x: ${cursorPos[id].x}px; --y: ${cursorPos[id].y}px`);
-        if (nextHoverState !== hoverState) {
-            if (nextHoverState)
-                cursors[id].setAttribute('data-hover', '');
-            else
-                cursors[id].removeAttribute('data-hover');
-        }
-    }
-
-    hoverState = nextHoverState;
-};
-
-let lerp = (src, dst, step) => {
-    return src + step * (dst - src);
-};
-
-let animateLerpCursors = (time) => {
-    // Process lerp-ed cursor positions
-    let needsUpdate = false;
-    for (let i = 0; i < lerpCursorIds.length; i++) {
-        let id = lerpCursorIds[i];
-        let nextX = lerp(cursorPos[id].x, pointerPos.x, 0.5 * Math.max(1, (time - lastUpdate) / 50));
-        let nextY = lerp(cursorPos[id].y, pointerPos.y, 0.5 * Math.max(1, (time - lastUpdate) / 50));
-
-        nextX = Math.abs(nextX - pointerPos.x) < 0.001 ? pointerPos.x : nextX;
-        nextY = Math.abs(nextY - pointerPos.y) < 0.001 ? pointerPos.y : nextY;
-
-        needsUpdate = needsUpdate || cursorPos[id].x !== nextX || cursorPos[id].y !== nextY;
-
-        cursorPos[id].x = nextX;
-        cursorPos[id].y = nextY;
-
-        if (!updateCursorIds.includes(id))
-            updateCursorIds.push(id);
-    }
-
-    if (needsUpdate)
-        requestRender();
-};
-
-let requestRender = () => {
-    rendererId = rendererId === 0 ? requestAnimationFrame((time) => {
-        updateCursors();
-        renderCursors();
-        rendererId = 0;
-        animateLerpCursors(time);
-        lastUpdate = time;
-    }) : rendererId;
-}
-
-let updateCursors = () => {
-    // Process normal cursor positions
-    for (let i = 0; i < cursorIds.length; i++) {
-        let id = cursorIds[i];
-        cursorPos[id].x = pointerPos.x;
-        cursorPos[id].y = pointerPos.y;
-        if (!updateCursorIds.includes(id))
-            updateCursorIds.push(id);
-    }
-};
-
-let pointerHandler = (event) => {
-    if (event.pointerType !== 'touch') {
-        pointerPos.x = event.clientX;
-        pointerPos.y = event.clientY;
-        nextCursorState = event.type !== 'pointerleave';
-        requestRender();
-    }
-};
-
-let hoverHandler = (event) => {
-    nextHoverState = event.type === 'pointerover';
-    requestRender();
-}
-
-document.documentElement.addEventListener('pointermove', pointerHandler, {passive: true});
-
-document.documentElement.addEventListener('pointerenter', pointerHandler, {passive: true});
-
-document.documentElement.addEventListener('pointerleave', pointerHandler, {passive: true});
-
-for (const elem of document.querySelectorAll('a, button')) {
-    elem.addEventListener('pointerover', hoverHandler, {passive: true});
-    elem.addEventListener('pointerout', hoverHandler, {passive: true});
 }
